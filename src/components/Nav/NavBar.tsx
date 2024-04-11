d71fea492b4b5a7e9481e19b4e45570ee83ce644
Giovanni Sansonetti <darkhuntshowdown@gmail.com>
2024-04-11 11:45:13 +0200

    libraries

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { validateRequest } from "~/server/auth"
 import Image from 'next/image'
 import UserMenu from "./UserMenu"
-import { redirect } from "next/navigation"
+import { CaretDown } from "react-bootstrap-icons";
 
 
 const NavBar = async() =>{   
@@ -11,16 +11,16 @@ const NavBar = async() =>{
 
     return(
         <div className="fixed w-full bg-[#181a1b] shadow-sm ">
-            <div className="px-4 md:px-16 py-6 flex flex-row items-center justify-between overflow-hidden">
+            <div className="px-4 md:px-16 py-8 flex flex-row items-center justify-between overflow-hidden">
                 {/*<Image src={} width={} height={}></Image> logo*/}
 
                 <div className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-10 text-white">
                     {session.session && session?.user?.userType === "DRIVER" ? 
-                        <ul>  
+                        <>  
                             <Link href={"/driver/createtrip"} className="">Create a new trip</Link>
                             <Link href={"/driver/createcar"} className="">Add a car</Link>
                             <Link href={"/driver"} className="">Dashboard</Link>
-                        </ul>
+                        </>
                         :
                         <>
                             <Link href={"/profile"} className="text-white">Your bookings</Link>
@@ -28,8 +28,8 @@ const NavBar = async() =>{
                             <Link href={"/driver"} className="text-white">Ratings</Link>
                         </>
                     }
-                    <div className="">
-                        <UserMenu username={session.user?.username}/>    
+                    <div className="flex items-center justify-center">
+                        <UserMenu username={session.user?.username}/><CaretDown />
                     </div>
                 </div>
             </div>   
